51c8624af3cf5ca00d093c743757cfe38f6ca737
g-otn <g-otn@hotmail.com>
2019-06-20 00:14:16 -0300

    Add commands calls

--- bot/commands/hue.js
+++ bot/commands/hue.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-module.exports = (msg) => msg.channel.send('br')
-=======
-exports.hue = (msg) => msg.channel.send('br')
->>>>>>> 06e286e1a37b415f7b788ceb163b84a63d27367f
+module.exports = (msg) => {
+    msg.channel.send('br')
+}
--- bot/index.js
+++ bot/index.js
@@ -7,8 +7,8 @@ bot.on("message", (msg) => {
     if (msg.author.bot || !msg.content.startsWith(process.env.PREFIX))
         return;
 
-    let cmd = msg.content.substring(0, msg.content.indexOf(' '))
-    let args = msg.content.indexOf(' ').split(' ').slice(1)
+    let cmd = msg.content.split(' ')[0].substring(process.env.PREFIX.length)
+    let args = msg.content.split(' ').slice(1)
     console.log('cmd:', cmd)
     console.log('args:', args)
 
@@ -17,18 +17,22 @@ bot.on("message", (msg) => {
             // require('./commands/forums')(bot, msg)
             break
         case 'help':
+            require('./commands/help')(msg, args)
             break
         case 'hue':
             require('./commands/hue')(msg)
             break
         case 'lb':
         case 'leaderboard':
+            require('./commands/leaderboard')(msg, args)
             break;
         case 'on':
         case 'online':
+            require('./commands/online')(msg, args)
             break
         case 'pop':
         case 'population':
+            require('./commands/population')(msg, args)
             break
         case 'server':
         case 'cs':
@@ -39,6 +43,7 @@ bot.on("message", (msg) => {
         case 'prophunt':
         case 'va':
         case 'vanilla':
+            require('./commands/server')(msg, cmd, args)
             break
         case 'serverh':
         case 'csh':
@@ -49,13 +54,17 @@ bot.on("message", (msg) => {
         case 'prophunth':
         case 'vah':
         case 'vanillah':
+            require('./commands/serverh')(msg, cmd, args)
+            break
         case 'stats':
+            require('./commands/stats')(msg, args)
             break
         case 'steam':
         case 'steaminfo':
+            require('./commands/steaminfo')(msg, args)
             break
         default:
     }
 })
-console.log(process.env)
+
 bot.login(process.env.TOKEN)
