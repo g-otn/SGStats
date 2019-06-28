ae544be6231bbb4d7315110165009e5d7966b28b
g-otn <g-otn@hotmail.com>
2019-06-28 06:07:49 -0300

    help main menu

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -6,13 +6,24 @@ async function sendHelpMenu(msg, command) {
     let helpmsg = new Discord.RichEmbed()
     .setFooter('SGStats v' + pkg.version + ' by ' + pkg.author, 'https://cdn.discordapp.com/avatars/310491216393404416/97be88722638646a0be55b1fcb65bf7c.png?size=32')
 
-    if (command && info[command]) { // Command exists
-        
+    if (command && info.commands[command]) { // Command exists
+        helpmsg
+        .setTitle('The ' + command + ' command')
+        .setDescription(info.commands[command].description)
+        if (info.commands[command].server) {
+
+        }
     } else {                        // Unknown or no command
         helpmsg
         .setTitle('SGStats Commands')
-        .setDescription('Use ``' + process.env.PREFIX + 'help <command>`` for help on a specific command')
         .setColor('BLUE')
+        let description = 'Use ``' + process.env.PREFIX + 'help <command>`` for help on a specific command\n'
+        info.categories.forEach(category => {
+            helpmsg.addField(category + ' commands', '```\n' + Object.keys(info.commands).filter(command => {
+                return info.commands[command].category == category
+            }).join('\n') + '\n```', false)
+        });
+        helpmsg.setDescription(description)
         msg.channel.send(helpmsg)
     }
 }
