2fd1cd390bcb7b53b2d138a1754ab75789da4f08
g-otn <g-otn@hotmail.com>
2019-07-02 02:59:56 -0300

    add gmod hours scraping

--- bot/commands/steaminfo.js
+++ bot/commands/steaminfo.js
@@ -15,17 +15,32 @@ async function getSteamInfo(steamID64) {
     }
     // Get user basic info
     options.uri = 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/',
+        await rp(options)
+            .catch(err => {
+                console.log(err)
+            })
+            .then(res => {
+                if (res.response.players.length !== 0)
+                    steamInfo = res.response.players[0]
+            })
+
+    // Get gmod hours
+    options.uri = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/`
+    options.qs.steamids = undefined
+    options.qs.steamid = steamID64
     await rp(options)
         .catch(err => {
             console.log(err)
         })
         .then(res => {
-            if (res.response.players.length !== 0)
-                steamInfo = res.response.players[0]
+            if (res.response.games) {
+                let gmodMinutes = res.response.games.find(game => {
+                    return game.appid == 4000
+                }).playtime_forever
+                if (gmodMinutes)
+                    steamInfo.gmodHours = Math.round(gmodMinutes / 60)
+            }
         })
-    
-    // TODO: Get gmod hours and other info
-    options.uri = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/`
 
     return steamInfo
 }
@@ -34,8 +49,8 @@ function sendMessage(msg, steamInfo) {
 
     msg.channel.send(
         new Discord.RichEmbed()
-        .setDescription(JSON.stringify(steamInfo, null, '\t'))
-        .setColor('GREY')
+            .setDescription(JSON.stringify(steamInfo, null, '\t'))
+            .setColor('GREY')
     )
 }
 
@@ -44,38 +59,38 @@ function sendSteamInfo(msg, input) {
     if (!input) {
         msg.channel.send(
             new Discord.RichEmbed()
-            .setTitle('Missing input')
-            .setDescription('You must type an input!\n' + commands.list.steaminfo.syntax[1] + '\nType ``' + process.env.PREFIX + 'help steaminfo`` for more information.')
-            .setThumbnail('https://cdn.glitch.com/bcfe2b58-fec3-47dd-9035-1ff2cfe59574%2Fk_giggle.png?v=1561883974179')
-            .setColor('RED')
+                .setTitle('Missing input')
+                .setDescription('You must type an input!\n' + commands.list.steaminfo.syntax[1] + '\nType ``' + process.env.PREFIX + 'help steaminfo`` for more information.')
+                .setThumbnail('https://cdn.glitch.com/bcfe2b58-fec3-47dd-9035-1ff2cfe59574%2Fk_giggle.png?v=1561883974179')
+                .setColor('RED')
         )
         return
     }
-    
+
     input = input.trim()
 
     let steamInfo
     if (input.match(/^STEAM_[0-5]:[01]:\d+$/)) { // SteamID
         getSteamInfo(steam.convertTo64(input))
-        .then(steamInfo => sendMessage(msg, steamInfo))
+            .then(steamInfo => sendMessage(msg, steamInfo))
     } else if (input.match(/^7656119\d{10}$/)) { // SteamID64 
         getSteamInfo(input)
-        .then(steamInfo => sendMessage(msg, steamInfo))
+            .then(steamInfo => sendMessage(msg, steamInfo))
     } else {                                     // CustomURL        
         steam.convertVanity(input, (err, res) => {
             if (err) { // Invalid CustomURL
                 msg.channel.send(
                     new Discord.RichEmbed()
-                    .setTitle('User not found')
-                    .setURL(`https://steamcommunity.com/id/${input}`)
-                    .setDescription(`No steam user with the custom URL of \"${input}\" was found.`)
-                    .setThumbnail('https://cdn.glitch.com/bcfe2b58-fec3-47dd-9035-1ff2cfe59574%2Fk_sad.png?v=1561883974814')
-                    .setColor('DARK_RED')
+                        .setTitle('User not found')
+                        .setURL(`https://steamcommunity.com/id/${input}`)
+                        .setDescription(`No steam user with the custom URL of \"${input}\" was found.`)
+                        .setThumbnail('https://cdn.glitch.com/bcfe2b58-fec3-47dd-9035-1ff2cfe59574%2Fk_sad.png?v=1561883974814')
+                        .setColor('DARK_RED')
                 )
                 return
             }
             getSteamInfo(res)
-            .then(steamInfo => sendMessage(msg, steamInfo))
+                .then(steamInfo => sendMessage(msg, steamInfo))
         })
     }
 }
