0ec1c48ccfd363e3e835f19de2ce669cfe86eb44
g-otn <g-otn@hotmail.com>
2019-07-16 22:45:55 -0300

    update scraping and message sending

--- bot/commands/online.js
+++ bot/commands/online.js
@@ -4,30 +4,32 @@ const cheerio = require('cheerio')
 const servers = require('../data/servers.json')
 const thumbs = require('../data/thumbnails.json')
 const getAvailableServers = require('./help').getAvailableServers
+const getGraphURL = require('./data').getGraphURL
 
 async function getOnlinePlayers(serverIP) {
     let options = {
         uri: `https://www.gametracker.com/server_info/${serverIP}/`,
-        headers: {
-            'User-Agent': 'Request-Promise'
-        }
+        headers: { 'User-Agent': 'Request-Promise' }
     }
     let onlinePlayers = []
     await rp(options)
         .then(html => {
             const $ = cheerio.load(html)
-            let selection = $('#HTML_online_players tr').slice(1)
-            if (selection) {
-                selection.each((i, elem) => {
-                    console.log(i, elem.text())
-                    onlinePlayers.push({
-                        rank: elem.children().eq(0).text(),
-                        name: elem.children().eq(1).text(),
-                        score: elem.children().eq(2).text(),
-                        time: elem.children().eq(3).text(),
-                    })
-                    console.log(onlinePlayers[onlinePlayers.length - 1])
-                })
+            let selection = $('#HTML_online_players tr').slice(1) // table rows without first row (column titles)
+            for (let i = 0; i < selection.length; i++) {
+                let rowCells = selection.eq(i).children()
+                let player = {
+                    rank: rowCells.eq(0).text().trim().replace('.', ''),
+                    name: rowCells.eq(1).text().trim(),
+                    score: rowCells.eq(2).text().trim(),
+                    time: rowCells.eq(3).text().trim().split(':'),
+                }
+
+                // Formats HH:MM:SS to HHh 
+                player.time = (isNaN(player.time[player.time.length - 3]) ? 0 : Number(player.time[player.time.length - 3]) * 60) + Number(player.time[player.time.length - 2])
+
+                if (player.name) // Discards rows with blank names
+                    onlinePlayers.push(player)
             }
         })
         .catch(err => onlinePlayers.err = err)
@@ -46,6 +48,7 @@ exports.sendOnline = (msg, server) => {
         )
         return
     }
+    server = server.toLowerCase() // Removes case sensitivity
     if (!getAvailableServers('online').some(avaliableServer => { return avaliableServer == '``' + server + '``' })) {
         msg.channel.send(
             new Discord.RichEmbed()
@@ -59,10 +62,33 @@ exports.sendOnline = (msg, server) => {
 
     getOnlinePlayers(servers[server].ip)
         .then(onlinePlayers => {
-            msg.channel.send(
-                new Discord.RichEmbed()
-                    .setDescription(JSON.stringify(onlinePlayers, null, '\t'))
-            )
+            if (onlinePlayers.err)
+                msg.channel.send(
+                    new Discord.RichEmbed()
+                        .setTitle('Error')
+                        .setDescription('Something happened while gathering the online players\n' + steamInfo.err)
+                        .setThumbnail(thumbs.sad)
+                        .setColor('DARK_RED')
+                )
+            else if (onlinePlayers.length > 0) {
+                onlinePlayers = onlinePlayers.sort((a, b) => { return a.time < b.time ? 1 : (a.time > b.time ? -1 : 0) })
+                msg.channel.send(
+                    new Discord.RichEmbed()
+                        .setDescription(`Showing ${servers[server].name} online players\n and population throughout the day. **[Join now!](https://sgstats.glitch.me/redirect?ip=${servers[server].ip})**`)
+                        .addField('Name', onlinePlayers.map(player => player.name).join('\n'), true)
+                        .addField('Time played', onlinePlayers.map(player => (player.time / 60 >= 1 ? Math.floor(player.time / 60) + 'h ' : '') + player.time % 60 + 'min').join('\n'), true)
+                        .setImage(getGraphURL('population', 'day', server))
+                        .setColor('GOLD')
+                )
+            }
+            else
+                msg.channel.send(
+                    new Discord.RichEmbed()
+                        .setTitle('No players online')
+                        .setDescription(`There are no players online. **[Join now!](https://sgstats.glitch.me/redirect?ip=${servers[server].ip})**`)
+                        .setImage(getGraphURL('population', 'day', server))
+                        .setColor('GOLD')
+                )
         })
 }
 
