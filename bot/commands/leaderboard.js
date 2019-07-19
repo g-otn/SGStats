06ec725e191ad766756b9ab0edb630a94c34ff45
g-otn <g-otn@hotmail.com>
2019-07-19 03:47:35 -0300

    update leaderboard syntax and examples

--- bot/commands/leaderboard.js
+++ bot/commands/leaderboard.js
@@ -11,7 +11,41 @@ async function getLeaderboard(serverIP, sortingMethod, player) {
 }
 
 exports.sendLeaderboard = (msg, server, sortingMethod, player) => {
+    console.log('server:', server, '\nsort:', sortingMethod, '\nplayer:', player)
+    // Data validation
+    if (!server) {
+        msg.channel.send(
+            new Discord.RichEmbed()
+                .setTitle('Missing server')
+                .setDescription('You must choose a server!\n**Servers:** ' + getAvailableServers('online').join(', ') + '\nType ``' + process.env.PREFIX + 'help online`` for more information.')
+                .setThumbnail(thumbs.giggle)
+                .setColor('RED')
+        )
+        return
+    }
+    if (!servers[server] || !servers[server].gamertrackerID) {
+        msg.channel.send(
+            new Discord.RichEmbed()
+                .setTitle('Invalid server')
+                .setDescription('\"' + server + '\" is not a valid server!\n**Servers:** ' + getAvailableServers('leaderboard').join(', ') + '\nType ``' + process.env.PREFIX + 'help leaderboard`` for more information.')
+                .setThumbnail(thumbs.confused)
+                .setColor('RED')
+        )
+        return
+    }
+    if (!sortingMethod) {
+        // Sorting method is optional, so only player name needs to be input, if player name is inserted and sorting method is ommited, part of player name is inside sortingMethod (args[0])
+        msg.channel.send(
+            new Discord.RichEmbed()
+                .setTitle('Missing player')
+                .setDescription('You must type a player name!\nType ``' + process.env.PREFIX + 'help leaderboard`` for more information.')
+                .setThumbnail(thumbs.giggle)
+                .setColor('RED')
+        )
+        return
+    }
 
+    // Data parsing
 }
 
 exports.getLeaderboard = getLeaderboard
