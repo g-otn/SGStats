d631836755020fe8ae9970ec215e836a96e7ada4
g-otn <g-otn@hotmail.com>
2019-07-19 20:41:17 -0300

    scrap more info of found player

--- bot/commands/player.js
+++ bot/commands/player.js
@@ -4,7 +4,7 @@ const Base64 = require('js-base64').Base64
 const rp = require('request-promise')
 const cheerio = require('cheerio')
 
-async function searchPlayer(type, server, period, player) {
+async function searchPlayer(graphType, server, period, player) {
     // Requests to search page instead of generating a direct url so it can work with similar names
     let foundPlayer = {}
     let options = {
@@ -17,25 +17,31 @@ async function searchPlayer(type, server, period, player) {
     await rp(options)
         .then(html => {
             let $ = cheerio.load(html)
-            let selection = $('.table_lst a[href^="/player"]')
-            foundPlayer.name = selection.eq(0).text().trim()
+            let firstRow = $('.table_lst tr:nth-child(2)').children() // First table row (excluding columns titles row)
+            foundPlayer = {
+                rank: firstRow.eq(0).text().trim(),
+                name: firstRow.eq(1).text().trim(),
+                score: firstRow.eq(3).text().trim(),
+                timePlayed: firstRow.eq(4).text().trim(),
+                scoreMin: firstRow.eq(5).text().trim()
+            }
             if (foundPlayer.name) {
-                foundPlayer.profile = 'https://www.gametracker.com' + selection.eq(0).attr('href')
-                if (period)
-                    foundPlayer.graphURL = `https://cache.gametracker.com/images/graphs/player_${type == 'h' ? 'time' : 'score'}.php?nameb64=${encodeUrl(Base64.encode(foundPlayer.name))}&host=${server.ip}&start=-1${period}`
+                foundPlayer.profile = 'https://www.gametracker.com' + firstRow.eq(1).children().eq(0).attr('href')
+                if (graphType && period)
+                    foundPlayer.graphURL = `https://cache.gametracker.com/images/graphs/player_${graphType == 'h' ? 'time' : 'score'}.php?nameb64=${encodeUrl(Base64.encode(foundPlayer.name))}&host=${server.ip}&start=-1${period}`
             }
         })
     return foundPlayer
 }
 
-exports.sendPlayerGraph = (msg, type, server, period, player) => {
+exports.sendPlayerGraph = (msg, graphType, server, period, player) => {
     // Data validation
     if (!period || (period.match(/^(day)$|^(week)$|^(month)$|^d$|^w$|^m$/) && !player)) {
         // Missing player name
         msg.channel.send(
             new Discord.RichEmbed()
                 .setTitle('Missing player')
-                .setDescription('You must type a player name!\nType ``' + process.env.PREFIX + 'help player' + type + '`` for more information.')
+                .setDescription('You must type a player name!\nType ``' + process.env.PREFIX + 'help player' + graphType + '`` for more information.')
                 .setThumbnail('https://cdn.glitch.com/bcfe2b58-fec3-47dd-9035-1ff2cfe59574%2Fk_giggle.png?v=1561883974179')
                 .setColor('RED')
         )
@@ -52,13 +58,13 @@ exports.sendPlayerGraph = (msg, type, server, period, player) => {
     let unencodedPlayerName = player
     player = encodeUrl(player)
 
-    searchPlayer(type, server, period, player)
+    searchPlayer(graphType, server, period, player)
         .then(foundPlayer => {
             // Send message
             if (foundPlayer.name)
                 msg.channel.send(
                     new Discord.RichEmbed()
-                        .setDescription(`Showing [${foundPlayer.name}](${foundPlayer.profile})'s ${type == 'h' ? 'activity' : 'score'} on\n[${server.name}](https://www.gametracker.com/server_info/${server.ip}) throughout the ${period == 'd' ? 'day' : period == 'w' ? 'week' : period == 'm' ? 'month' : period}.\nFor similar names, click [here](https://www.gametracker.com/server_info/${server.ip}/top_players/?query=${player}).`)
+                        .setDescription(`Showing [${foundPlayer.name}](${foundPlayer.profile})'s ${graphType == 'h' ? 'activity' : 'score'} on\n[${server.name}](https://www.gametracker.com/server_info/${server.ip}) throughout the ${period == 'd' ? 'day' : period == 'w' ? 'week' : period == 'm' ? 'month' : period}.\nFor similar names, click [here](https://www.gametracker.com/server_info/${server.ip}/top_players/?query=${player}).`)
                         .setImage(foundPlayer.graphURL)
                         .setColor('BLUE')
                 )
