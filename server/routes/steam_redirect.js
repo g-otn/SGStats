d9b66801968b8c1face52ee8afe8ce3a24e16c6c
g-otn <g-otn@hotmail.com>
2019-07-23 18:04:30 -0300

    add views rendering and fix redirecting

--- server/routes/steam_redirect.js
+++ server/routes/steam_redirect.js
@@ -1,6 +1,12 @@
 var express = require('express')
 var app = module.exports = express()
+const servers = require('../../bot/data/servers.json')
 
-app.get('/redirect/:ip', (req, res) => {
-    res.redirect('steam://connect/' + req.params.ip)
+app.get('/redirect/:serverKey', (req, res) => {
+    let server = req.params.serverKey
+    
+    if (servers[server])
+        res.render('../server/views/redirect.pug', servers[server])
+    else
+        res.render('../server/views/invalid.pug', { invalidServer: server, servers: servers })
 })
